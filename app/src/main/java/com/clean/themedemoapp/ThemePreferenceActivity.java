a41cc0653821d8ac8a710656c68a77790779e691
Sunil <sdubey@hifinite.com>
2019-06-14 16:46:12 +0530

    theme customization

---
+++ app/src/main/java/com/clean/themedemoapp/ThemePreferenceActivity.java
@@ -0,0 +1,42 @@
+package com.clean.themedemoapp;
+
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class ThemePreferenceActivity extends PreferenceActivity {
+	public static final int RESULT_CODE_THEME_UPDATED = 1;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String themeName = pref.getString("theme", "Theme1");
+        if (themeName.equals("Theme1")) {
+            setTheme(R.style.Theme1);
+        } else if (themeName.equals("Theme2")) {
+            setTheme(R.style.Theme2);
+        }
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+		findPreference("theme").setOnPreferenceChangeListener(new RefershActivityOnPreferenceChangeListener(RESULT_CODE_THEME_UPDATED));
+	}
+	
+    private class RefershActivityOnPreferenceChangeListener implements OnPreferenceChangeListener {
+        private final int resultCode;
+        public RefershActivityOnPreferenceChangeListener(int resultCode) {
+            this.resultCode = resultCode;
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference p, Object newValue) {
+            setResult(resultCode);
+            finish();
+            startActivity(getIntent());
+            return true;
+        }
+    }
+}
